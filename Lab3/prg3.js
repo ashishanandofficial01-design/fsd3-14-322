07eb52fd9c5d36c19aba04e03b94ca9c00fa297c
ashishanandofficial01-ctrl <ashishanandofficial01@gmail.com>
2026-08-20 14:49:31 +0530

    send response

---
+++ Lab3/prg3.js
@@ -0,0 +1,29 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        res.end("<h1>Home Page</h1>");
+
+    } else if (req.url === "/about") {
+        res.end("<h1>About Page</h1>");
+
+    } else if (req.url === "/product") {
+        res.end(`
+            <h1>Mobile Phones</h1>
+            <h2>Price:50,000</h2>
+            <p>Discount: 5%</p>
+            <a href="#">Buy Now</a>
+        `);
+
+    } else {
+        res.statusCode = 404;
+        res.end(`
+            <h1>404 Not Found</h1>
+            <h2>Page Not Found</h2>
+        `);
+    }
+});
+
+server.listen(4444, () => {
+    console.log("Server is running on port 4444");
+});
