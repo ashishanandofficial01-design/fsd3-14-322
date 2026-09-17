fc98e17fbfbe2141029d0b3ced2de81350e8f254
ashishanandofficial01-ctrl <ashishanandofficial01@gmail.com>
2026-09-17 12:56:07 +0530

    lab5 start 15 sept

---
+++ lab5/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello Express</h1>");
+});
+
+app.listen(3000, () => console.log("...........Server is running on PORT 3000.........."));
